0afc4153a22151868d9947d53f70f573e77464fd
DavidLuna0 <ddavidr569@gmail.com>
2019-08-13 07:57:53 -0300

    Configurando mapa

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux'
+import MapView from 'react-native-maps';
 
 export class Home extends Component {
 
@@ -17,14 +18,38 @@ export class Home extends Component {
     }
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLocation: {
+        latitude: -23.7,
+        longitude: -46.8,
+        latitudeDelta: 0.004,
+        longitudeDelta: 0.004,
+      }
+    }
+  }
+/* MapView recebe duas props principaisa  regiao inicial e o style */
   render() {
     return(
-        <View>
-            <Text>Tela Home</Text>
+        <View style={styles.container}>
+          
+            <MapView style={styles.map} initialRegion={this.state.currentLocation}>
+
+            </MapView>
         </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    flex: 1
+  }
+})
 
 const mapStateToProps = (state) => {
   return {
